56cf37bcde9ffc28fe43bdbbc6346a5a6357b8d0
RajatRawat10 <rajatrawat4242@gmail.com>
2025-12-24 10:48:00 +0530

    flash niddleware

--- Connect flash middleware/app.js
+++ Connect flash middleware/app.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const session = require("express-session");
+const flash = require("connect-flash");
+
 // with connect flash we can save the data in the route and can use in the other route
+app.use(session({
+    resave:false,
+    saveUninitialized:false,
+    secret:"anything we can do here"
+}));
+app.use(flash());
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
