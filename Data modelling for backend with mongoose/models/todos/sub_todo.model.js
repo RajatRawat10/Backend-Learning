3254ebd29f7337cf7a3b8f0e3c1ba44b9ee6b196
RajatRawat10 <rajatrawat4242@gmail.com>
2025-12-28 19:00:46 +0530

    data modeling

--- Data modelling for backend with mongoose/models/todos/sub_todo.model.js
+++ Data modelling for backend with mongoose/models/todos/sub_todo.model.js
@@ -1,5 +1,24 @@
+import { type } from "express/lib/response";
 import mongoose from "mongoose";
 
-const Userschema = new mongoose.Schema({});
+const Userschema = new mongoose.Schema({
+  username: {
+    type: String,
+    reqiured: true,
+    unique: true,
+    lowercase: true,
+  },
+  email: {
+    type: String,
+    reqiured: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    reqiured: true,
+  },
+},{timestamps:true}
+);
 
 export const User = mongoose.model("User", Userschema);
