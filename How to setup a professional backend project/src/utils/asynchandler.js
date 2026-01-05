a3b47cd11ad9d60d38d070f82b10f1a61a177cd8
RajatRawat10 <rajatrawat4242@gmail.com>
2026-01-05 21:37:18 +0530

    error handling

---
+++ How to setup a professional backend project/src/utils/asynchandler.js
@@ -0,0 +1,22 @@
+// this is the promise approch
+
+const asyncHandler = (func) => {
+  (req, res, next) => {
+    Promise.resolve(func(req, res, next)).reject((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// this is the try catch approach
+
+// const asyncHandler = (func) => async (req, res, next) => {
+//   try {
+//     await func(req, res, next);
+//   } catch (error) {
+//     res.status(err.code || 500).json({
+//       success: false,
+//       message: err.message,
+//     });
+//   }
+// };
