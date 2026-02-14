5ef1853d86744711bdb1eeccec296a5799bc2bd9
RajatRawat10 <rajatrawat4242@gmail.com>
2026-02-15 00:41:01 +0530

    model of user is created

---
+++ Authentication Practice/src/model/user.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.model(
+  {
+    username: {
+      type: string,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    mobile: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
